0122098b14ffeccfbd0e4cb55ed864903ec9bd49
Randy <randy86v@gmail.com>
2013-01-08 22:18:52 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Olca::Application.config.secret_token = '49b98a499fbdd336cca098460ef8401bdce9ae5c274d9bd8f2b74a621134c38a4ceb6d24b0dd58388368121b382632d3debcab4f55f6a01d49971c97834382e5'
